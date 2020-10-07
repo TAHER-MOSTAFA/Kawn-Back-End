8035bd37964d5799b346251a80b4b4d8b8fbc7a9
tata <taher.mostafa808@gmail.com>
2020-10-07 19:42:54 +0000

    fix fb-auth

--- kawen/settings.py
+++ kawen/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'graphene_django',
-    #'social_django',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -68,8 +68,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                #'social_django.context_processors.backends',
-                #'social_django.context_processors.login_redirect',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
@@ -115,8 +115,8 @@ GRAPHENE = {
     ],
 }
 AUTHENTICATION_BACKENDS = [
-    #'social_core.backends.facebook.FacebookOAuth2',
-    #'graphql_jwt.backends.JSONWebTokenBackend',
+    'social_core.backends.facebook.FacebookOAuth2',
+    'graphql_jwt.backends.JSONWebTokenBackend',
     'django.contrib.auth.backends.ModelBackend',
 ]
 
