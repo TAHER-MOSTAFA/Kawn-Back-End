65fca0ab08b54bf7c8521ed6117b9aa0b2114dcd
TAHER-MOSTAFA <taher.mostafa808@gmail.com>
2021-05-28 05:49:47 +0200

    Add redis for channel_layer

--- kawen/settings.py
+++ kawen/settings.py
@@ -118,8 +118,14 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
-CHANNEL_LAYERS = {"default": {"BACKEND": "channels.layers.InMemoryChannelLayer"}}
-
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis", 6379)],
+        },
+    },
+}
 GRAPHENE = {
     "SCHEMA": "kawen.schema.schema",
     "MIDDLEWARE": ["graphql_jwt.middleware.JSONWebTokenMiddleware"],
