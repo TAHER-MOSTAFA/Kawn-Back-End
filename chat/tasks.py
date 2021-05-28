2a033bb7470c5b0ab080dccb60305cc236f0e7ba
TAHER-MOSTAFA <taher.mostafa808@gmail.com>
2021-05-28 21:50:03 +0200

    hot fix write to db if cached is seen

--- chat/tasks.py
+++ chat/tasks.py
@@ -1,3 +1,5 @@
+from django.core.cache import cache
+
 from kawen.celery import app
 
 from .models import UserMessage
@@ -6,5 +8,9 @@ from .models import UserMessage
 @app.task
 def write_user_missed_messages_to_db(msg_id, users_ids):
     UserMessage.objects.bulk_create(
-        [UserMessage(user_id=user_id, message_id=msg_id) for user_id in users_ids]
+        [
+            UserMessage(user_id=user_id, message_id=msg_id)
+            for user_id in users_ids
+            if cache.get(f"{user_id}_{msg_id}") != None
+        ]
     )
